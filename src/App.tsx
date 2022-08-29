db1471e7e813ba9c0dbb175774169310c4e69857
diliphiremath <dilip.s.hiremath@gmail.com>
2022-08-30 00:56:11 +0530

    reolve eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
 import RoutesList from './common/RoutesList';
 import { BrowserRouter as Router } from 'react-router-dom';
 
